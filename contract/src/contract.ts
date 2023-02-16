0f78ccb271d4274a692f56d49b606e1621a7e373
Sam <samuel.waters@gmail.com>
2023-02-16 10:07:24 -0700

    second

--- contract/src/contract.ts
+++ contract/src/contract.ts
@@ -5,10 +5,12 @@ import { NearBindgen, near, call, view } from 'near-sdk-js';
 class HelloNear {
   message: string = "Hello";
 
+
   @view({}) // This method is read-only and can be called for free
   get_greeting(): string {
     return this.message;
   }
+
 
   @call({}) // This method changes the state, for which it cost gas
   set_greeting({ message }: { message: string }): void {
